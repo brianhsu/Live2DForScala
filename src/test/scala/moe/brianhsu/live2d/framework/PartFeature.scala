cf37764f1704c8e4a56e2c4c31ca5b49a1466aba
Brian Hsu <brianhsu.hsu@gmail.com>
2021-03-14 15:52:20 +0800

    add: Add unit test for Part.

---
+++ src/test/scala/moe/brianhsu/live2d/framework/PartFeature.scala
@@ -0,0 +1,35 @@
+package moe.brianhsu.live2d.framework
+
+import moe.brianhsu.live2d.utils.NativeMemoryUtils
+import org.scalatest.GivenWhenThen
+import org.scalatest.featurespec.AnyFeatureSpec
+import org.scalatest.matchers.should.Matchers
+
+class PartFeature extends AnyFeatureSpec with GivenWhenThen with Matchers {
+  Feature("Accessing opacity data from C Pointer") {
+    Scenario("Reading opacity value from C memory") {
+      Given("a pointer to a C memory that is a float number")
+      val expectedOpacity = 123.456f
+      val pointer = NativeMemoryUtils.createPointerToFloat(expectedOpacity)
+
+      When("create a Part from that pointer")
+      val part = Part(pointer, "partId", None)
+
+      Then("it should able to read correct opacity value")
+      part.opacity shouldBe expectedOpacity
+    }
+
+    Scenario("Write opacity value to C memory") {
+      Given("a Part associated with a C memory")
+      val pointer = NativeMemoryUtils.createPointerToFloat(0)
+      val part = Part(pointer, "partId", None)
+
+      When("set opacity of a Part")
+      part.setOpacity(567.123f)
+
+      Then("the value of the native memory should have the updated value")
+      pointer.getFloat(0) shouldBe 567.123f
+    }
+  }
+
+}
