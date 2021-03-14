fc0218046ddb8886ce2c92788da100141e28de4e
Brian Hsu <brianhsu.hsu@gmail.com>
2021-03-14 16:06:42 +0800

    add: Add unit test for parts.

--- src/test/scala/moe/brianhsu/live2d/framework/Live2DModelFeature.scala
+++ src/test/scala/moe/brianhsu/live2d/framework/Live2DModelFeature.scala
@@ -1,10 +1,13 @@
 package moe.brianhsu.live2d.framework
 
-import org.scalatest.{GivenWhenThen, Inside}
+import org.scalatest.{GivenWhenThen, Inside, OptionValues}
 import org.scalatest.featurespec.AnyFeatureSpec
 import org.scalatest.matchers.should.Matchers
 
-class Live2DModelFeature extends AnyFeatureSpec with GivenWhenThen with Matchers with Inside {
+import scala.io.Source
+
+class Live2DModelFeature extends AnyFeatureSpec with GivenWhenThen with Matchers with Inside with OptionValues {
+
   private val modelFile = "src/test/resources/models/HaruGreeter/runtime/haru_greeter_t03.moc3"
   private val cubism = new Cubism
 
@@ -13,7 +16,7 @@ class Live2DModelFeature extends AnyFeatureSpec with GivenWhenThen with Matchers
       Given("A Live2D HaruGreeter Model")
       val model = cubism.loadModel(modelFile)
 
-      When("Query the canvas info")
+      When("Get the canvas info")
       val canvasInfo = model.canvasInfo
 
       Then("it should return the correct canvas info")
@@ -23,6 +26,29 @@ class Live2DModelFeature extends AnyFeatureSpec with GivenWhenThen with Matchers
         originX shouldBe 1200
         originY shouldBe 2250
         pixelPerUnit shouldBe 2400
+      }
+    }
+  }
+
+  Feature("Get parts data") {
+    Scenario("reading parts data from model") {
+      Given("A Live2D HaruGreeter Model")
+      val model = cubism.loadModel(modelFile)
+
+      When("Get the parts")
+      val parts = model.parts
+
+      Then("it should have correct number of parts")
+      val expectedParts = Source.fromResource("expectation/PartIdList.txt").getLines().toList
+      parts.size shouldBe expectedParts.size
+
+      And("all expected part id should have corresponding Part object")
+      expectedParts.foreach { partId =>
+        val part = parts.get(partId)
+        part.value shouldBe a[Part]
+        part.value.id shouldBe partId
+        part.value.parentIdHolder shouldBe None
+        part.value.opacity shouldBe 1.0f
       }
     }
   }
