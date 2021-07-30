4fb990b47897dad9d77459647b0b739cd0f95fd6
Brian Hsu <brianhsu@gmail.com>
2021-07-30 14:28:05 +0800

    bugfix: Fix compile error.

--- src/main/scala/moe/brianhsu/live2d/adapter/gateway/model/CubismModelBackend.scala
+++ src/main/scala/moe/brianhsu/live2d/adapter/gateway/model/CubismModelBackend.scala
@@ -243,7 +243,7 @@ class CubismModelBackend(mocInfo: MocInfo, override val textureFiles: List[Strin
       val opacityPointer = opacityList.getPointerToFloat(i)
       val maskCount = maskCountList(i)
       val masks = (0 until maskCount).toList.map(j => masksList(i)(j))
-      val vertexInfo = drawable.VertexInfo(
+      val vertexInfo = VertexInfo(
         vertexCountList(i),
         indexCountList(i),
         positionList(i),
