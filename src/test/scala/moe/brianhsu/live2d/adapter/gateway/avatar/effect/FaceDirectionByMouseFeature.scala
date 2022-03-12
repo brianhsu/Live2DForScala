edd362820faba5c78062ced02f3a20d9bffc5a65
Brian Hsu <brianhsu.hsu@gmail.com>
2022-03-12 20:46:19 +0800

    modify: Change test data to improve test coverage.

--- src/test/scala/moe/brianhsu/live2d/adapter/gateway/avatar/effect/FaceDirectionByMouseFeature.scala
+++ src/test/scala/moe/brianhsu/live2d/adapter/gateway/avatar/effect/FaceDirectionByMouseFeature.scala
@@ -11,26 +11,29 @@ class FaceDirectionByMouseFeature extends AnyFeatureSpec with GivenWhenThen with
   case class TimedData(interval: Float, viewX: Float, viewY: Float, expectedTargetX: Float, expectedTargetY: Float)
 
   Feature("The face direction target point calculator should calculate correct face target point") {
-    Given("An sequence of interval / viewX / viewY data")
-    val testDataList = loadTestData
+    Scenario("Doing a sequence of mouse drag") {
+      Given("An sequence of interval / viewX / viewY data")
+      val testDataList = loadTestData
 
-    Then("it should calculate correct expectedTargetX / expectedTargetY from test data")
-    val targetPointCalculator = new FaceDirectionByMouse(30)
-    var totalElapsedTimeInSeconds = 0.0f
+      Then("it should calculate correct expectedTargetX / expectedTargetY from test data")
+      val targetPointCalculator = new FaceDirectionByMouse(30)
+      var totalElapsedTimeInSeconds = 0.0f
 
-    for (row <- testDataList) {
-      totalElapsedTimeInSeconds += row.interval
-      targetPointCalculator.setFaceTargetCoordinate(row.viewX, row.viewY)
-      targetPointCalculator.updateFrameTimeInfo(totalElapsedTimeInSeconds, row.interval)
-      val (faceX, faceY) = targetPointCalculator.currentFaceCoordinate
-      faceX shouldBe row.expectedTargetX
-      faceY shouldBe row.expectedTargetY
+      for (row <- testDataList) {
+        totalElapsedTimeInSeconds += row.interval
+        targetPointCalculator.setFaceTargetCoordinate(row.viewX, row.viewY)
+        targetPointCalculator.updateFrameTimeInfo(totalElapsedTimeInSeconds, row.interval)
+        val (faceX, faceY) = targetPointCalculator.currentFaceCoordinate
+
+        faceX shouldBe row.expectedTargetX +- 0.0000000001f
+        faceY shouldBe row.expectedTargetY +- 0.0000000001f
+      }
     }
   }
 
   private def loadTestData: List[TimedData] = {
-    val lines = Source.fromResource("effects/FaceDirectionTargetPointData.csv").getLines().toList
-    lines.map { line =>
+    val lines = Source.fromResource("expectation/faceDirectionTargetPointData.csv").getLines().toList
+    lines.drop(1).map { line =>
       val Array(interval, viewX, viewY, expectedTargetX, expectedTargetY) = line.split(" ")
       TimedData(interval.toFloat, viewX.toFloat, viewY.toFloat, expectedTargetX.toFloat, expectedTargetY.toFloat)
     }
