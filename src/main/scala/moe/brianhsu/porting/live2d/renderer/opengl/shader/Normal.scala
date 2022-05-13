839ec5600620191728c099abbcde2589be504b50
Brian Hsu <brianhsu.hsu@gmail.com>
2022-05-13 17:30:56 +0800

    modify: Reformat source code in Shaders.

--- src/main/scala/moe/brianhsu/porting/live2d/renderer/opengl/shader/Normal.scala
+++ src/main/scala/moe/brianhsu/porting/live2d/renderer/opengl/shader/Normal.scala
@@ -4,30 +4,33 @@ import moe.brianhsu.live2d.enitiy.opengl.OpenGLBinding
 import moe.brianhsu.live2d.usecase.renderer.shader.AvatarShader
 
 class Normal(implicit gl: OpenGLBinding) extends AvatarShader {
-  override def vertexShaderSource: String = {
-    "#version 120\n" +
-    "attribute vec4 a_position;" +
-    "attribute vec2 a_texCoord;" +
-    "varying vec2 v_texCoord;" +
-    "uniform mat4 u_matrix;" +
-    "void main()" +
-    "{" +
-    "gl_Position = u_matrix * a_position;" +
-    "v_texCoord = a_texCoord;" +
-    "v_texCoord.y = 1.0 - v_texCoord.y;" +
-    "}"
+  override def vertexShaderSource: String =
+    """|#version 120
+       |
+       |attribute vec4 a_position;
+       |attribute vec2 a_texCoord;
+       |
+       |varying vec2 v_texCoord;
+       |uniform mat4 u_matrix;
+       |
+       |void main() {
+       |  gl_Position = u_matrix * a_position;
+       |  v_texCoord = a_texCoord;
+       |  v_texCoord.y = 1.0 - v_texCoord.y;
+       |}
+       |""".stripMargin
 
-  }
-
-  override def fragmentShaderSource: String = {
-    "#version 120\n" +
-    "varying vec2 v_texCoord;" + //v2f.texcoord
-    "uniform sampler2D s_texture0;" + //_MainTex
-    "uniform vec4 u_baseColor;" + //v2f.color
-    "void main()" +
-    "{" +
-    "vec4 color = texture2D(s_texture0 , v_texCoord) * u_baseColor;" +
-    "gl_FragColor = vec4(color.rgb * color.a,  color.a);" +
-    "}"
-  }
+  override def fragmentShaderSource: String =
+    """|#version 120
+       |
+       |varying vec2 v_texCoord;
+       |
+       |uniform sampler2D s_texture0;
+       |uniform vec4 u_baseColor;
+       |
+       |void main() {
+       |  vec4 color = texture2D(s_texture0 , v_texCoord) * u_baseColor;
+       |  gl_FragColor = vec4(color.rgb * color.a,  color.a);
+       |}
+       |""".stripMargin
 }
--- src/main/scala/moe/brianhsu/porting/live2d/renderer/opengl/shader/SetupMask.scala
+++ src/main/scala/moe/brianhsu/porting/live2d/renderer/opengl/shader/SetupMask.scala
@@ -4,37 +4,43 @@ import moe.brianhsu.live2d.enitiy.opengl.OpenGLBinding
 import moe.brianhsu.live2d.usecase.renderer.shader.AvatarShader
 
 class SetupMask(implicit gl: OpenGLBinding) extends AvatarShader {
-  override def vertexShaderSource: String = {
-    "#version 120\n" +
-      "attribute vec4 a_position;" +
-      "attribute vec2 a_texCoord;" +
-      "varying vec2 v_texCoord;" +
-      "varying vec4 v_myPos;" +
-      "uniform mat4 u_clipMatrix;" +
-      "void main()" +
-      "{" +
-      "gl_Position = u_clipMatrix * a_position;" +
-      "v_myPos = u_clipMatrix * a_position;" +
-      "v_texCoord = a_texCoord;" +
-      "v_texCoord.y = 1.0 - v_texCoord.y;" +
-      "}"
-  }
+  override def vertexShaderSource: String =
+    """|#version 120
+       |
+       |attribute vec4 a_position;
+       |attribute vec2 a_texCoord;
+       |
+       |varying vec2 v_texCoord;
+       |varying vec4 v_myPos;
+       |
+       |uniform mat4 u_clipMatrix;
+       |
+       |void main() {
+       |  gl_Position = u_clipMatrix * a_position;
+       |  v_myPos = u_clipMatrix * a_position;
+       |  v_texCoord = a_texCoord;
+       |  v_texCoord.y = 1.0 - v_texCoord.y;
+       |}
+       |""".stripMargin
 
-  override def fragmentShaderSource: String = {
-    "#version 120\n" +
-      "varying vec2 v_texCoord;" +
-      "varying vec4 v_myPos;" +
-      "uniform sampler2D s_texture0;" +
-      "uniform vec4 u_channelFlag;" +
-      "uniform vec4 u_baseColor;" +
-      "void main()" +
-      "{" +
-      "float isInside = " +
-      "  step(u_baseColor.x, v_myPos.x/v_myPos.w)" +
-      "* step(u_baseColor.y, v_myPos.y/v_myPos.w)" +
-      "* step(v_myPos.x/v_myPos.w, u_baseColor.z)" +
-      "* step(v_myPos.y/v_myPos.w, u_baseColor.w);" +
-      "gl_FragColor = u_channelFlag * texture2D(s_texture0 , v_texCoord).a * isInside;" +
-      "}"
-  }
+  override def fragmentShaderSource: String =
+    """|#version 120
+       |
+       |varying vec2 v_texCoord;
+       |varying vec4 v_myPos;
+       |
+       |uniform sampler2D s_texture0;
+       |uniform vec4 u_channelFlag;
+       |uniform vec4 u_baseColor;
+       |
+       |void main() {
+       |  float isInside =
+       |    step(u_baseColor.x, v_myPos.x/v_myPos.w)
+       |      * step(u_baseColor.y, v_myPos.y/v_myPos.w)
+       |      * step(v_myPos.x/v_myPos.w, u_baseColor.z)
+       |      * step(v_myPos.y/v_myPos.w, u_baseColor.w);
+       |
+       |  gl_FragColor = u_channelFlag * texture2D(s_texture0 , v_texCoord).a * isInside;
+       |}
+       |""".stripMargin
 }
