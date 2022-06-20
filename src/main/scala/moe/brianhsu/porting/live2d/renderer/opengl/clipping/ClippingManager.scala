43d7ba4ab91b1e1d51aa29a9f44daa040c451c87
Brian Hsu <brianhsu.hsu@gmail.com>
2022-06-20 21:51:42 +0800

    modify: Remove code that is impossible to execute.

--- src/main/scala/moe/brianhsu/porting/live2d/renderer/opengl/clipping/ClippingManager.scala
+++ src/main/scala/moe/brianhsu/porting/live2d/renderer/opengl/clipping/ClippingManager.scala
@@ -32,83 +32,78 @@ class ClippingManager(model: Live2DModel, textureManager: TextureManager)(implic
     contextList.sortBy(_.clippedDrawables.head.index)
   }
 
-  def setupLayoutBounds(usingClipCount: Int): Unit = {
-    if (usingClipCount <= 0) {
-      contextListForMask = contextListForMask.map(_.copy(layout = Layout(0, Rectangle(0.0f, 0.0f, 1.0f, 1.0f))))
-    } else {
+  private def setupLayoutBounds(usingClipCount: Int): Unit = {
+    // ひとつのRenderTextureを極力いっぱいに使ってマスクをレイアウトする
+    // マスクグループの数が4以下ならRGBA各チャンネルに１つずつマスクを配置し、5以上6以下ならRGBAを2,2,1,1と配置する
 
-      // ひとつのRenderTextureを極力いっぱいに使ってマスクをレイアウトする
-      // マスクグループの数が4以下ならRGBA各チャンネルに１つずつマスクを配置し、5以上6以下ならRGBAを2,2,1,1と配置する
+    val div = usingClipCount / ClippingContext.ColorChannelCount //１チャンネルに配置する基本のマスク個数
+    val mod = usingClipCount % ClippingContext.ColorChannelCount //余り、この番号のチャンネルまでに１つずつ配分する
 
-      val div = usingClipCount / ClippingContext.ColorChannelCount //１チャンネルに配置する基本のマスク個数
-      val mod = usingClipCount % ClippingContext.ColorChannelCount //余り、この番号のチャンネルまでに１つずつ配分する
+    // RGBAそれぞれのチャンネルを用意していく(0:R , 1:G , 2:B, 3:A, )
+    var curClipIndex = 0 //順番に設定していく
 
-      // RGBAそれぞれのチャンネルを用意していく(0:R , 1:G , 2:B, 3:A, )
-      var curClipIndex = 0 //順番に設定していく
+    for (channelNo <- 0 until ClippingContext.ColorChannelCount) {
+      // このチャンネルにレイアウトする数
+      val layoutCount = div + (if (channelNo < mod) 1 else 0)
 
-      for (channelNo <- 0 until ClippingContext.ColorChannelCount) {
-        // このチャンネルにレイアウトする数
-        val layoutCount = div + (if (channelNo < mod) 1 else 0)
-
-        layoutCount match {
-          case 0 =>
-          case 1 =>
+      layoutCount match {
+        case 0 =>
+        case 1 =>
+          val cc = contextListForMask(curClipIndex)
+          contextListForMask = contextListForMask.updated(
+            curClipIndex,
+            cc.copy(layout = Layout(channelNo, Rectangle(0.0f, 0.0f, 1.0f, 1.0f)))
+          )
+          curClipIndex += 1
+        case 2 =>
+          //UVを2つに分解して使う
+          for (i <- 0 until layoutCount) {
+            val xPosition = i % 2
             val cc = contextListForMask(curClipIndex)
             contextListForMask = contextListForMask.updated(
               curClipIndex,
-              cc.copy(layout = Layout(channelNo, Rectangle(0.0f, 0.0f, 1.0f, 1.0f)))
+              cc.copy(layout = Layout(channelNo, Rectangle(xPosition * 0.5f, 0.0f, 0.5f, 1.0f)))
             )
             curClipIndex += 1
-          case 2 =>
-            //UVを2つに分解して使う
-            for (i <- 0 until layoutCount) {
-              val xPosition = i % 2
-              val cc = contextListForMask(curClipIndex)
-              contextListForMask = contextListForMask.updated(
-                curClipIndex,
-                cc.copy(layout = Layout(channelNo, Rectangle(xPosition * 0.5f, 0.0f, 0.5f, 1.0f)))
-              )
-              curClipIndex += 1
-            }
-          case 3|4 =>
-            //4分割して使う
-            for (i <- 0 until layoutCount) {
-              val xPosition = i % 2
-              val yPosition = i / 2
-              val cc = contextListForMask(curClipIndex)
-              contextListForMask = contextListForMask.updated(
-                curClipIndex,
-                cc.copy(layout = Layout(channelNo, Rectangle(xPosition * 0.5f, yPosition * 0.5f, 0.5f, 0.5f)))
-              )
-              curClipIndex += 1
-            }
-          case 5|6|7|8|9 =>
-            //9分割して使う
-            for (i <- 0 until layoutCount) {
-              val xPosition = i % 3
-              val yPosition = i / 3
-              val cc = contextListForMask(curClipIndex)
+          }
+        case 3 | 4 =>
+          //4分割して使う
+          for (i <- 0 until layoutCount) {
+            val xPosition = i % 2
+            val yPosition = i / 2
+            val cc = contextListForMask(curClipIndex)
+            contextListForMask = contextListForMask.updated(
+              curClipIndex,
+              cc.copy(layout = Layout(channelNo, Rectangle(xPosition * 0.5f, yPosition * 0.5f, 0.5f, 0.5f)))
+            )
+            curClipIndex += 1
+          }
+        case 5 | 6 | 7 | 8 | 9 =>
+          //9分割して使う
+          for (i <- 0 until layoutCount) {
+            val xPosition = i % 3
+            val yPosition = i / 3
+            val cc = contextListForMask(curClipIndex)
 
-              contextListForMask = contextListForMask.updated(
-                curClipIndex,
-                cc.copy(layout = Layout(channelNo, Rectangle(xPosition / 3.0f, yPosition / 3.0f, 1.0f / 3.0f, 1.0f / 3.0f)))
-              )
-              curClipIndex += 1
-            }
-          case _ =>
-            printf("not supported mask count : %d\n", layoutCount)
+            contextListForMask = contextListForMask.updated(
+              curClipIndex,
+              cc.copy(layout = Layout(channelNo, Rectangle(xPosition / 3.0f, yPosition / 3.0f, 1.0f / 3.0f, 1.0f / 3.0f)))
+            )
+            curClipIndex += 1
+          }
+        case _ =>
+          printf("not supported mask count : %d\n", layoutCount)
 
-            // 引き続き実行する場合、 SetupShaderProgramでオーバーアクセスが発生するので仕方なく適当に入れておく
-            // もちろん描画結果はろくなことにならない
-            for (_ <- 0 until layoutCount) {
-              val cc = contextListForMask(curClipIndex)
-              contextListForMask = contextListForMask.updated(
-                curClipIndex,
-                cc.copy(layout = Layout(0, Rectangle(0.0f, 0.0f, 1.0f, 1.0f)))
-              )
-              curClipIndex += 1
-            }
-        }
+          // 引き続き実行する場合、 SetupShaderProgramでオーバーアクセスが発生するので仕方なく適当に入れておく
+          // もちろん描画結果はろくなことにならない
+          for (_ <- 0 until layoutCount) {
+            val cc = contextListForMask(curClipIndex)
+            contextListForMask = contextListForMask.updated(
+              curClipIndex,
+              cc.copy(layout = Layout(0, Rectangle(0.0f, 0.0f, 1.0f, 1.0f)))
+            )
+            curClipIndex += 1
+          }
       }
     }
   }
@@ -116,6 +111,7 @@ class ClippingManager(model: Live2DModel, textureManager: TextureManager)(implic
   def setupClippingContext(renderer: Renderer, lastFBO: Int, lastViewport: ViewPort): Unit = {
 
     this.contextListForMask = contextListForMask.map(_.calculateClippedDrawTotalBounds())
+
     val usingClipCount = contextListForMask.count(_.isUsing)
 
     // マスク作成処理
@@ -125,7 +121,6 @@ class ClippingManager(model: Live2DModel, textureManager: TextureManager)(implic
       renderer.preDraw()
       renderer.offscreenBufferHolder.foreach(_.beginDraw(lastFBO))
       setupLayoutBounds(usingClipCount)
-
 
       // 実際にマスクを生成する
       // 全てのマスクをどの様にレイアウトして描くかを決定し、ClipContext , ClippedDrawContext に記憶する
