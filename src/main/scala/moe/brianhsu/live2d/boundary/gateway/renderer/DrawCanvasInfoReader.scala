0491c8d7d384b9da439c99a21fe2bc88a82c8d7c
Brian Hsu <brian.hsu@yahooinc.com>
2022-06-24 12:41:39 +0800

    modify: Fix HDPI in MacOS.

--- src/main/scala/moe/brianhsu/live2d/boundary/gateway/renderer/DrawCanvasInfoReader.scala
+++ src/main/scala/moe/brianhsu/live2d/boundary/gateway/renderer/DrawCanvasInfoReader.scala
@@ -3,4 +3,7 @@ package moe.brianhsu.live2d.boundary.gateway.renderer
 trait DrawCanvasInfoReader {
   def currentCanvasWidth: Int
   def currentCanvasHeight: Int
+  def currentSurfaceWidth: Int
+  def currentSurfaceHeight: Int
+
 }
--- src/main/scala/moe/brianhsu/porting/live2d/demo/LAppView.scala
+++ src/main/scala/moe/brianhsu/porting/live2d/demo/LAppView.scala
@@ -104,7 +104,7 @@ class LAppView(drawCanvasInfo: DrawCanvasInfoReader)(private implicit val openGL
       drawCanvasInfo.currentCanvasHeight
     )
 
-    openGL.glViewport(0, 0, drawCanvasInfo.currentCanvasWidth, drawCanvasInfo.currentCanvasHeight)
+    openGL.glViewport(0, 0, drawCanvasInfo.currentSurfaceWidth, drawCanvasInfo.currentSurfaceHeight)
     backgroundSprite.resize()
     powerSprite.resize()
     gearSprite.resize()
