e1b170ce279808ba02f3a67664b9c0cf13a5c18a
GitHub Action <brianhsu.hsu+githu@gmail.com>
2022-07-20 07:34:35 +0000

    add: Add bundle packages in GitHub actions.

--- scripts/packageBundles.sh
+++ scripts/packageBundles.sh
@@ -6,25 +6,25 @@ version=`git tag | tail -n1`
 rm -rvf openSeeFace.tar.gz openSeeFace Live2DForScala-Swing/ Live2DForScala-SWT-Linux/ Live2DForScala-SWT-Win/
 
 curl --output openSeeFace.tar.gz https://brianhsu.moe/openSeeFace.tar.gz
-tar -xvzf openSeeFace.tar.gz
+tar -xzf openSeeFace.tar.gz
 
 mkdir Live2DForScala-Swing
-cp -rvf openSeeFace Live2DForScala-Swing/
+cp -rf openSeeFace Live2DForScala-Swing/
 cp -v modules/examples/swing/target/scala-2.13/Live2DForScala-Swing-*.jar Live2DForScala-Swing/
 
 mkdir Live2DForScala-SWT-Linux
-cp -rvf openSeeFace Live2DForScala-SWT-Linux/
+cp -rf openSeeFace Live2DForScala-SWT-Linux/
 cp -v modules/examples/swt-linux-bundle/target/scala-2.13/Live2DForScala-SWT-Linux-*.jar Live2DForScala-SWT-Linux/
 
-mkdir Live2DForScala-SWT-Win
-cp -rvf openSeeFace Live2DForScala-SWT-Win/
-cp -v modules/examples/swt-windows-bundle/target/scala-2.13/Live2DForScala-SWT-Windows-*.jar Live2DForScala-SWT-Win/
+mkdir Live2DForScala-SWT-Windows
+cp -rf openSeeFace Live2DForScala-SWT-Windows
+cp -v modules/examples/swt-windows-bundle/target/scala-2.13/Live2DForScala-SWT-Windows-*.jar Live2DForScala-SWT-Windows
 
-zip -r Live2DForScala-Swing-$version.zip Live2DForScala-Swing/
-zip -r Live2DForScala-SWT-Win-$version.zip Live2DForScala-SWT-Win/
-tar -cvzf Live2DForScala-SWT-Linux-$version.tar.gz Live2DForScala-SWT-Linux/
+zip -rq Live2DForScala-Swing-$version.zip Live2DForScala-Swing/
+zip -rq Live2DForScala-SWT-Windows-$version.zip Live2DForScala-SWT-Windows
+tar -czf Live2DForScala-SWT-Linux-$version.tar.gz Live2DForScala-SWT-Linux/
 
-rm -rvf openSeeFace.tar.gz openSeeFace Live2DForScala-Swing/ Live2DForScala-SWT-Linux/ Live2DForScala-SWT-Win/
-
+rm -rf openSeeFace.tar.gz openSeeFace Live2DForScala-Swing/ Live2DForScala-SWT-Linux/ Live2DForScala-SWT-Win/
+pwd
 ls -l
 
